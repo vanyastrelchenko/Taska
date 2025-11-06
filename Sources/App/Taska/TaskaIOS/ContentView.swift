aa55ae12b78e71d5e4f2e8da81b9699216ab05e1
Ivan Strelchenko <107491110+vanyastrelchenko@users.noreply.github.com>
2025-11-07 00:11:51 +0700

    Chore/flatten sources (#6)
    * chore: repo initial structure (templates, CODEOWNERS, CONTRIBUTING, .editorconfig, gitignore)
    
    * ci: run only on pull_request (remove push context)
    
    * chore: retrigger CI
    
    * chore: flatten Sources/App structure and add minimal Package.swift
    
    * Remove obsolete files and add new project structure for Taska. Deleted CONTRIBUTING.md, LICENSE, Package.swift, and README.md. Introduced new Xcode project files and asset catalogs for iOS. Updated ContentView and TaskaApp file paths.

---
+++ Sources/App/Taska/TaskaIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taska
+//
+//  Created by Ivan Strelchenko on 2025-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sources/App/Taska/TaskaIOS/TaskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskaApp.swift
+//  Taska
+//
+//  Created by Ivan Strelchenko on 2025-11-06.
+//
+
+import SwiftUI
+
+@main
+struct TaskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
